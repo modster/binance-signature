2d6386bff67f4491762b8729e7ea0652f71b85b3
E. M. Greeff <emgreeff@datadash.cloud>
2023-05-31 19:13:42 -0600

    Deno branch,  see main for nodejs

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const crypto = require('crypto');
+// const crypto = require("crypto");
+const { createHmac } = await import("node:crypto");
 
-const query_string = 'timestamp=1578963600000';
-const apiSecret = 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j';
-
+const query_string = "timestamp=1578963600000";
+// const apiSecret =
+// "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j";
+import "https://deno.land/std@0.177.1/dotenv/load.ts";
+// const log = console.log;
+const apiSecret = Deno.env.get("API_SECRET");
+// const APIKEY = Deno.env.get("TESTNET_APIKEY");
 function signature(query_string) {
-    return crypto
-        .createHmac('sha256', apiSecret)
-        .update(query_string)
-        .digest('hex');
+  return createHmac("sha256", apiSecret)
+    .update(query_string)
+    .digest("hex");
 }
 
 console.log("hashing the string: ");
@@ -17,7 +21,8 @@ console.log(signature(query_string));
 
 console.log("\n");
 
-const another_query = 'symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559';
+const another_query =
+  "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559";
 console.log("hashing the string: ");
 console.log(another_query);
 console.log("and return:");
