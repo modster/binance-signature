3952caebabd749274c4cdf3740677d30b3ad3744
E. M. Greeff <emgreeff@datadash.cloud>
2023-05-31 19:07:25 -0600

    deno

---
+++ main.js
@@ -0,0 +1,29 @@
+// const crypto = require("crypto");
+const { createHmac } = await import("node:crypto");
+
+const query_string = "timestamp=1578963600000";
+// const apiSecret =
+// "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j";
+import "https://deno.land/std@0.177.1/dotenv/load.ts";
+// const log = console.log;
+const apiSecret = Deno.env.get("API_SECRET");
+// const APIKEY = Deno.env.get("TESTNET_APIKEY");
+function signature(query_string) {
+  return createHmac("sha256", apiSecret)
+    .update(query_string)
+    .digest("hex");
+}
+
+console.log("hashing the string: ");
+console.log(query_string);
+console.log("and return:");
+console.log(signature(query_string));
+
+console.log("\n");
+
+const another_query =
+  "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559";
+console.log("hashing the string: ");
+console.log(another_query);
+console.log("and return:");
+console.log(signature(another_query));
